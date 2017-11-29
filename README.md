64de6afd80fdce28460c8c299bbb4dac4c54eced
Daniel Bradley <daniel@webelegant.com>
2017-11-29 13:01:04 +1000

    Fixed mysterious semi-colon issue by outputing dud characters to dev null.

--- README.md
+++ README.md
@@ -132,7 +132,7 @@ main
            |-- processPreformatted
 ```
 
-An summary of these functions is given, then their implementation is discussed in detail.
+A summary of these functions is given, then their implementation is discussed in detail.
 
 ```main.c
 int main( int argc, char** argv );
@@ -210,37 +210,52 @@ Otherwise, each file passed in the arguments is passed to the 'tryToProcess' fun
 along with the *line delimiter*.
 
 ```main.c
-int main( int argc, char** files )
+int main( int argc, char** argv )
 {
-	char* pat = argumentsGetValue( argc, files, "-p" );
-	STRIP     = argumentsContains( argc, files, "-s" ) ? 1 : 0;
+    char* pat = argumentsGetValue( argc, argv, "-p" );
+    STRIP     = argumentsContains( argc, argv, "-s" ) ? 1 : 0;
 
                                         // 1.       2.  3.  4.            5.
-	int expected_arguments = 4 + STRIP; // extract [-s] -p "some pattern" file...
+    int expected_arguments = 4 + STRIP; // extract [-s] -p "some pattern" file...
 
-	if ( ! pat )
-	{
-		exit( usage() );
-	}
-	else
-	if ( argc < expected_arguments )
-	{
-		exit( usage() );
-	}
-	else
-	{
-		char* line_delimiter = generateDelimiter( pat );
-		int i;
-		for ( i=expected_arguments; i < argc; i++ )
-		{
-			tryToProcess( files[i], pattern );
-		}
+    if ( ! pat )
+    {
+        exit( usage() );
+    }
+    else
+    if ( argc < expected_arguments )
+    {
+        exit( usage() );
+    }
+    else
+    {
+        char* line_delimiter = generateDelimiter( pat );
 
-		free( pattern );
-	}
-	free( pat );
+        //
+        //  Must try to process each arg that does not start with '-', except:
+        //  argv[0], which is process name, and
+        //  argv[?], pattern which follows '-p'.
+        //
 
-	return 0;
+        int i;
+        for ( i = 1; i < argc; i++ )
+        {
+            if ( '-' != argv[i][0] )
+            {
+                tryToProcess( argv[i], line_delimiter );
+            }
+            else
+            if ( 'p' == argv[i][1] )
+            {
+                i++;
+            }
+        }
+
+        free( line_delimiter );
+    }
+    free( pat );
+
+    return 0;
 }
 ```
 
@@ -249,18 +264,18 @@ int main( int argc, char** files )
 ```main.c
 int argumentsContains( int n, char** files, char* flag )
 {
-	int b = 0;
-	int i;
+    int b = 0;
+    int i;
 
-	for ( i=0; i < n; i++ )
-	{
-		if ( stringEquals( flag, files[i] ) )
-		{
-			b = 1;
-			i = n;
-		}
-	}
-	return b;
+    for ( i=0; i < n; i++ )
+    {
+        if ( stringEquals( flag, files[i] ) )
+        {
+            b = 1;
+            i = n;
+        }
+    }
+    return b;
 }
 ```
 
@@ -269,19 +284,19 @@ int argumentsContains( int n, char** files, char* flag )
 ```main.c
 char* argumentsGetValue( int n, char** files, char* flag )
 {
-	char* ret = NULL;
-	int i;
+    char* ret = NULL;
+    int i;
 
-	for ( i=0; i < n; i++ )
-	{
-		if ( stringEquals( flag, files[i] ) )
-		{
-			i++;
-			if ( i < n) ret = strdup( files[i] );
-			i = n;
-		}
-	}
-	return ret;
+    for ( i=0; i < n; i++ )
+    {
+        if ( stringEquals( flag, files[i] ) )
+        {
+            i++;
+            if ( i < n) ret = strdup( files[i] );
+            i = n;
+        }
+    }
+    return ret;
 }
 ```
 
@@ -290,8 +305,8 @@ char* argumentsGetValue( int n, char** files, char* flag )
 ```main.c
 int usage()
 {
-	fprintf( stderr, "Usage:\n\textract [-s] -p <pattern> <file> [more files] [ > output file ]\n" );
-	return -1;
+    fprintf( stderr, "Usage:\n\textract [-s] -p <pattern> <file> [more files] [ > output file ]\n" );
+    return -1;
 }
 ```
 
@@ -300,13 +315,13 @@ int usage()
 ```main.c
 char* generateDelimiter( const char* pat )
 {
-	char* delimiter = calloc( strlen( pat ) + 3, sizeof(char) );
-	char* tmp       = delimiter;
-	      tmp       = stpcpy( tmp, "~" );
-	      tmp       = stpcpy( tmp, pat );
-	      tmp       = stpcpy( tmp, "~" );
+    char* delimiter = calloc( strlen( pat ) + 3, sizeof(char) );
+    char* tmp       = delimiter;
+          tmp       = stpcpy( tmp, "~" );
+          tmp       = stpcpy( tmp, pat );
+          tmp       = stpcpy( tmp, "~" );
 
-	return delimiter;
+    return delimiter;
 }
 ```
 
@@ -315,13 +330,13 @@ char* generateDelimiter( const char* pat )
 ```main.c
 void tryToProcess( char* file, const char* pattern )
 {
-	FILE* stream;
-	if ( (stream = fopen( file, "r" )) )
-	{
-		if ( DEBUG ) fprintf( stderr, "Processing: %s\n", file );
-		process( stream, pattern );
-		fclose( stream );
-	}
+    FILE* stream;
+    if ( (stream = fopen( file, "r" )) )
+    {
+        if ( DEBUG ) fprintf( stderr, "Processing: %s\n", file );
+        process( stream, pattern );
+        fclose( stream );
+    }
 }
 ```
 
@@ -330,24 +345,24 @@ void tryToProcess( char* file, const char* pattern )
 ```main.c
 void process( FILE* stream, const char* pattern )
 {
-	char* line;
-	do
-	{
-		line = readline( stream );
-		if ( line )
-		{
-			switch ( line[0] )
-			{
-			case '~':
-				if ( DEBUG ) fprintf( stderr, "@%s", line );
-				processPreformatted( line, stream, pattern );
-				break;
-			default:
-				if ( DEBUG ) fprintf( stderr, "@%s", line );
-			}
-			free( line );
-		}
-	} while ( line );
+    char* line;
+    do
+    {
+        line = readline( stream );
+        if ( line )
+        {
+            switch ( line[0] )
+            {
+            case '~':
+                if ( DEBUG ) fprintf( stderr, "@%s", line );
+                processPreformatted( line, stream, pattern );
+                break;
+            default:
+                if ( DEBUG ) fprintf( stderr, "@%s", line );
+            }
+            free( line );
+        }
+    } while ( line );
 }
 ```
 
@@ -356,67 +371,70 @@ void process( FILE* stream, const char* pattern )
 ```main.c
 void processPreformatted( char* line, FILE* stream, const char* pattern )
 {
-	int   loop = 1;
-	char* pre;
+    int   loop = 1;
+    char* pre;
 
-	FILE* out  = stderr;
-	char* c    = "@";
+    FILE* dev_null = fopen( "/dev/null", "a" );
+    FILE* out      = dev_null;
+    char* c        = "@";
 
-	if ( 0 == strncmp( pattern, line, strlen(pattern) ) )
-	{
-		out = stdout;
-		c   = "";
-	}
+    if ( 0 == strncmp( pattern, line, strlen(pattern) ) )
+    {
+        out = stdout;
+        c   = "";
+    }
 
-	do
-	{
-		pre = readline( stream );
-		if ( pre )
-		{
-			if ( STRIP )
-			{
-				switch ( pre[0] )
-				{
-				case '~':
-					if ( DEBUG ) fprintf( stderr, "@%s", pre );
-					loop = 0;
-					break;
-				case '/':
-					if ( '/' == pre[1] ) fprintf( out, ";\n" );
-					break;
-				case 'D':
-				case 'd':
-					if ( 0 == strncmp( "DROP",      pre, strlen( "DROP"      ) ) ) break;
-					if ( 0 == strncmp( "drop",      pre, strlen( "drop"      ) ) ) break;
-					if ( 0 == strncmp( "DELIMITER", pre, strlen( "DELIMITER" ) ) ) break;
-					if ( 0 == strncmp( "delimiter", pre, strlen( "delimiter" ) ) ) break;
-				default:
-					if ( 0 != strncmp( "@", c, 1 ) ) fprintf( out, "%s%s", c, pre );
-				}
-			}
-			else
-			{
-				switch ( pre[0] )
-				{
-				case '~':
-					if ( DEBUG ) fprintf( stderr, "@%s", pre );
-					loop = 0;
-					break;
-				case 'D':
-				case 'd':
-					if ( 0 == strncmp( "DROP",      pre, strlen( "DROP"      ) ) ) break;
-					if ( 0 == strncmp( "drop",      pre, strlen( "drop"      ) ) ) break;
-					if ( 0 == strncmp( "delimiter", pre, strlen( "delimiter" ) ) ) break;
-				default:
-					if ( 0 != strncmp( "@", c, 1 ) ) fprintf( out, "%s%s", c, pre );
-				}
-			}
-			free( pre );
-		} else {
-			loop = 0;
-		}
-	}
-	while ( loop );
+    do
+    {
+        pre = readline( stream );
+        if ( pre )
+        {
+            if ( STRIP )
+            {
+                switch ( pre[0] )
+                {
+                case '~':
+                    if ( DEBUG ) fprintf( stderr, "@%s", pre );
+                    loop = 0;
+                    break;
+                case '/':
+                    if ( '/' == pre[1] ) fprintf( out, ";\n" );
+                    break;
+                case 'D':
+                case 'd':
+                    if ( 0 == strncmp( "DROP",      pre, strlen( "DROP"      ) ) ) break;
+                    if ( 0 == strncmp( "drop",      pre, strlen( "drop"      ) ) ) break;
+                    if ( 0 == strncmp( "DELIMITER", pre, strlen( "DELIMITER" ) ) ) break;
+                    if ( 0 == strncmp( "delimiter", pre, strlen( "delimiter" ) ) ) break;
+                default:
+                    if ( 0 != strncmp( "@", c, 1 ) ) fprintf( out, "%s%s", c, pre );
+                }
+            }
+            else
+            {
+                switch ( pre[0] )
+                {
+                case '~':
+                    if ( DEBUG ) fprintf( stderr, "@%s", pre );
+                    loop = 0;
+                    break;
+                case 'D':
+                case 'd':
+                    if ( 0 == strncmp( "DROP",      pre, strlen( "DROP"      ) ) ) break;
+                    if ( 0 == strncmp( "drop",      pre, strlen( "drop"      ) ) ) break;
+                    if ( 0 == strncmp( "delimiter", pre, strlen( "delimiter" ) ) ) break;
+                default:
+                    if ( 0 != strncmp( "@", c, 1 ) ) fprintf( out, "%s%s", c, pre );
+                }
+            }
+            free( pre );
+        } else {
+            loop = 0;
+        }
+    }
+    while ( loop );
+
+    fclose( dev_null );
 }
 ```
 
@@ -425,37 +443,37 @@ void processPreformatted( char* line, FILE* stream, const char* pattern )
 ```main.c
 char* readline( FILE* stream )
 {
-	int   n = 0;
-	char* line = calloc( 1024, sizeof(char) );
-	char* ptr  = calloc( 10,  sizeof(char) );
-	int read;
-	do
-	{
-		read = fread( ptr, sizeof(char), 1, stream );
-		if ( read )
-		{
-			switch( *ptr )
-			{
-			case '\n':
-				line[n++] = *ptr;
-				read = 0;
-				break;
-			default:
-				line[n++] = *ptr;
-			}
-		}
-	} while ( 0 != read );
-	free( ptr );
+    int   n = 0;
+    char* line = calloc( 1024, sizeof(char) );
+    char* ptr  = calloc( 10,  sizeof(char) );
+    int read;
+    do
+    {
+        read = fread( ptr, sizeof(char), 1, stream );
+        if ( read )
+        {
+            switch( *ptr )
+            {
+            case '\n':
+                line[n++] = *ptr;
+                read = 0;
+                break;
+            default:
+                line[n++] = *ptr;
+            }
+        }
+    } while ( 0 != read );
+    free( ptr );
 
-	if ( n )
-	{
-		return line;
-	}
-	else
-	{
-		free( line );
-		return NULL;
-	}
+    if ( n )
+    {
+        return line;
+    }
+    else
+    {
+        free( line );
+        return NULL;
+    }
 }
 ```
 
@@ -464,7 +482,7 @@ char* readline( FILE* stream )
 ```main.c
 int stringEquals( const char* one, const char* two )
 {
-	return (0 == strcmp( one, two ));
+    return (0 == strcmp( one, two ));
 }
 ```
 
@@ -475,6 +493,6 @@ int stringHasPrefix( const char* string, const char* prefix )
 {
     int len = strlen( prefix )
 
-	return (0 == strcmp( string, prefix, len ));
+    return (0 == strcmp( string, prefix, len ));
 }
 ```
--- extract/source/src/extract.c
+++ extract/source/src/extract.c
@@ -1,24 +1,24 @@
 /*
- *		Extract
- *
- *		This is a generalisatin of the extract SQL program -- its purpose
- *		is to extract pre-formatted segments from MaxText documentation
- *		source files.
- *
- *		If the first argument is "-p", the second argument is treated as a
- *		"pattern" specifier. The pattern matched is '~' . <pattern> . '~'.
- *
- *		Further arguments to the command-line are treated as MaxText files. Each
- *		file is (read-only) processed in turn and output to the standard out.
- *
- *		For each file lines are output to stdout if they existing between a
- *		line that starts with the specified pattern and a line that starts with "~".
- *
- *		Usage:
- *
- *			extract -p "sql" <MaxText Files> [ > output file]
- *
- */
+*        Extract
+*
+*        This is a generalisatin of the extract SQL program -- its purpose
+*        is to extract pre-formatted segments from MaxText documentation
+*        source files.
+*
+*        If the first argument is "-p", the second argument is treated as a
+*        "pattern" specifier. The pattern matched is '~' . <pattern> . '~'.
+*
+*        Further arguments to the command-line are treated as MaxText files. Each
+*        file is (read-only) processed in turn and output to the standard out.
+*
+*        For each file lines are output to stdout if they existing between a
+*        line that starts with the specified pattern and a line that starts with "~".
+*
+*        Usage:
+*
+*            extract -p "sql" <MaxText Files> [ > output file]
+*
+*/
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -28,6 +28,7 @@ static int DEBUG = 0;
 
 int    argumentsContains();
 char*  argumentsGetValue();
+int         stringEquals( const char* one, const char* two );
 int                usage();
 char*    generatePattern( const char* );
 void        tryToProcess( char*, const char* );
@@ -37,223 +38,241 @@ void processPreformatted( char* line, FILE*, const char* pattern );
 
 int STRIP;
 
-int main( int argc, char** files )
+int main( int argc, char** argv )
 {
-	STRIP     = argumentsContains( argc, files, "-s" ) ? 1 : 0;
-	char* pat = argumentsGetValue( argc, files, "-p" );
+    STRIP     = argumentsContains( argc, argv, "-s" ) ? 1 : 0;
+    char* pat = argumentsGetValue( argc, argv, "-p" );
 
-	int expected_arguments = 4 + STRIP; // extract -p "some pattern" files [-s]
+    int expected_arguments = 4 + STRIP; // extract -p "some pattern" files [-s]
 
-	if ( ! pat )
-	{
-		return usage();
-	}
-	else
-	if ( argc < expected_arguments )
-	{
-		return usage();
-	}
-	else
-	{
-		char* pattern = generatePattern( pat );
-		int i;
-		for ( i=expected_arguments; i < argc; i++ )
-		{
-			tryToProcess( files[i], pattern );
-		}
+    if ( ! pat )
+    {
+        return usage();
+    }
+    else
+    if ( argc < expected_arguments )
+    {
+        return usage();
+    }
+    else
+    {
+        char* line_delimiter = generateDelimiter( pat );
 
-		free( pattern );
-	}
-	free( pat );
+        //
+        //  Must try to process each arg that does not start with '-', except:
+        //  argv[0], which is process name, and
+        //  argv[?], pattern which follows '-p'.
+        //
 
-	return 0;
+        int i;
+        for ( i = 1; i < argc; i++ )
+        {
+            if ( '-' != argv[i][0] )
+            {
+                tryToProcess( argv[i], pattern );
+            }
+            else
+            if ( 'p' == argv[i][1] )
+            {
+                i++;
+            }
+        }
+
+        free( line_delimiter );
+    }
+    free( pat );
+
+    return 0;
 }
 
-	int argumentsContains( int n, char** files, char* flag )
-	{
-		int b = 0;
-		int i;
+int argumentsContains( int n, char** files, char* flag )
+{
+    int b = 0;
+    int i;
 
-		for ( i=0; i < n; i++ )
-		{
-			if ( stringEquals( flag, files[i] ) )
-			{
-				b = 1;
-				i = n;
-			}
-		}
-		return b;
-	}
+    for ( i=0; i < n; i++ )
+    {
+        if ( stringEquals( flag, files[i] ) )
+        {
+            b = 1;
+            i = n;
+        }
+    }
+    return b;
+}
 
-	char* argumentsGetValue( int n, char** files, char* flag )
-	{
-		char* ret = NULL;
-		int i;
+char* argumentsGetValue( int n, char** files, char* flag )
+{
+    char* ret = NULL;
+    int i;
 
-		for ( i=0; i < n; i++ )
-		{
-			if ( stringEquals( flag, files[i] ) )
-			{
-				i++;
-				if ( i < n) ret = strdup( files[i] ); 
-				i = n;
-			}
-		}
-		return ret;
-	}
+    for ( i=0; i < n; i++ )
+    {
+        if ( stringEquals( flag, files[i] ) )
+        {
+            i++;
+            if ( i < n) ret = strdup( files[i] );
+            i = n;
+        }
+    }
+    return ret;
+}
 
-	int stringEquals( const char* one, const char* two )
-	{
-		return (0 == strcmp( one, two ));
-	}
+int stringEquals( const char* one, const char* two )
+{
+    return (0 == strcmp( one, two ));
+}
 
-	int usage()
-	{
-		fprintf( stderr, "Usage:\n\textract [-s] -p <pattern> <file> [more files] [ > output file ]\n" );
-		return -1;
-	}
+int usage()
+{
+    fprintf( stderr, "Usage:\n\textract [-s] -p <pattern> <file> [more files] [ > output file ]\n" );
+    return -1;
+}
 
-	char* generatePattern( const char* pat )
-	{
-		int len = strlen( pat );
-		char* pattern = calloc( len + 3, sizeof(char) );
-		char* tmp = pattern;
-		tmp = stpcpy( tmp, "~" );
-		tmp = stpcpy( tmp, pat );
-		tmp = stpcpy( tmp, "~" );
-		return pattern;
-	}
+char* generateDelimiter( const char* pat )
+{
+char* delimiter = calloc( strlen( pat ) + 3, sizeof(char) );
+char* tmp       = delimiter;
+      tmp       = stpcpy( tmp, "~" );
+      tmp       = stpcpy( tmp, pat );
+      tmp       = stpcpy( tmp, "~" );
 
-	void tryToProcess( char* file, const char* pattern )
-	{
-		FILE* stream;
-		if ( (stream = fopen( file, "r" )) )
-		{
-			if ( DEBUG ) fprintf( stderr, "Processing: %s\n", file );
-			process( stream, pattern );
-			fclose( stream );
-		}
-	}
+return delimiter;
+}
 
-		void process( FILE* stream, const char* pattern )
-		{
-			char* line;
-			do
-			{
-				line = readline( stream );
-				if ( line )
-				{
-					switch ( line[0] )
-					{
-					case '~':
-						if ( DEBUG ) fprintf( stderr, "@%s", line );
-						processPreformatted( line, stream, pattern );
-						break;
-					default:
-						if ( DEBUG ) fprintf( stderr, "@%s", line );
-					}
-					free( line );
-				}
-			} while ( line );
-		}
+void tryToProcess( char* file, const char* pattern )
+{
+    FILE* stream;
+    if ( (stream = fopen( file, "r" )) )
+    {
+        if ( DEBUG ) fprintf( stderr, "Processing: %s\n", file );
+        process( stream, pattern );
+        fclose( stream );
+    }
+}
 
-			void processPreformatted( char* line, FILE* stream, const char* pattern )
-			{
-				int   loop = 1;
-				char* pre;
+    void process( FILE* stream, const char* pattern )
+    {
+        char* line;
+        do
+        {
+            line = readline( stream );
+            if ( line )
+            {
+                switch ( line[0] )
+                {
+                case '~':
+                    if ( DEBUG ) fprintf( stderr, "@%s", line );
+                    processPreformatted( line, stream, pattern );
+                    break;
+                default:
+                    if ( DEBUG ) fprintf( stderr, "@%s", line );
+                }
+                free( line );
+            }
+        } while ( line );
+    }
 
-				FILE* out  = stderr;
-				char* c    = "@";
+        void processPreformatted( char* line, FILE* stream, const char* pattern )
+        {
+            int   loop = 1;
+            char* pre;
 
-				if ( 0 == strncmp( pattern, line, strlen(pattern) ) )
-				{
-					out = stdout;
-					c   = "";
-				}
+            FILE* dev_null = fopen( "/dev/null", "a" );
+            FILE* out      = dev_null;
+            char* c        = "@";
 
-				do
-				{
-					pre = readline( stream );
-					if ( pre )
-					{
-						if ( STRIP )
-						{
-							switch ( pre[0] )
-							{
-							case '~':
-								if ( DEBUG ) fprintf( stderr, "@%s", pre );
-								loop = 0;
-								break;
-							case '/':
-								if ( '/' == pre[1] ) fprintf( out, ";\n" );
-								break;
-							case 'D':
-							case 'd':
-								if ( 0 == strncmp( "DROP",      pre, strlen( "DROP"      ) ) ) break;
-								if ( 0 == strncmp( "drop",      pre, strlen( "drop"      ) ) ) break;
-								if ( 0 == strncmp( "DELIMITER", pre, strlen( "DELIMITER" ) ) ) break;
-								if ( 0 == strncmp( "delimiter", pre, strlen( "delimiter" ) ) ) break;
-							default:
-								if ( 0 != strncmp( "@", c, 1 ) ) fprintf( out, "%s%s", c, pre );
-							}
-						}
-						else
-						{
-							switch ( pre[0] )
-							{
-							case '~':
-								if ( DEBUG ) fprintf( stderr, "@%s", pre );
-								loop = 0;
-								break;
-							case 'D':
-							case 'd':
-								if ( 0 == strncmp( "DROP",      pre, strlen( "DROP"      ) ) ) break;
-								if ( 0 == strncmp( "drop",      pre, strlen( "drop"      ) ) ) break;
-								if ( 0 == strncmp( "delimiter", pre, strlen( "delimiter" ) ) ) break;
-							default:
-								if ( 0 != strncmp( "@", c, 1 ) ) fprintf( out, "%s%s", c, pre );
-							}
-						}
-						free( pre );
-					} else {
-						loop = 0;
-					}
-				}
-				while ( loop );
-			}
+            if ( 0 == strncmp( pattern, line, strlen(pattern) ) )
+            {
+                out = stdout;
+                c   = "";
+            }
 
-				char* readline( FILE* stream )
-				{
-					int   n = 0;
-					char* line = calloc( 1024, sizeof(char) );
-					char* ptr  = calloc( 10,  sizeof(char) );
-					int read;
-					do
-					{
-						read = fread( ptr, sizeof(char), 1, stream );
-						if ( read )
-						{
-							switch( *ptr )
-							{
-							case '\n':
-								line[n++] = *ptr;
-								read = 0;
-								break;
-							default:
-								line[n++] = *ptr;
-							}
-						}
-					} while ( 0 != read );
-					free( ptr );
+            do
+            {
+                pre = readline( stream );
+                if ( pre )
+                {
+                    if ( STRIP )
+                    {
+                        switch ( pre[0] )
+                        {
+                        case '~':
+                            if ( DEBUG ) fprintf( stderr, "@%s", pre );
+                            loop = 0;
+                            break;
+                        case '/':
+                            if ( '/' == pre[1] ) fprintf( out, ";\n" );
+                            break;
+                        case 'D':
+                        case 'd':
+                            if ( 0 == strncmp( "DROP",      pre, strlen( "DROP"      ) ) ) break;
+                            if ( 0 == strncmp( "drop",      pre, strlen( "drop"      ) ) ) break;
+                            if ( 0 == strncmp( "DELIMITER", pre, strlen( "DELIMITER" ) ) ) break;
+                            if ( 0 == strncmp( "delimiter", pre, strlen( "delimiter" ) ) ) break;
+                        default:
+                            if ( 0 != strncmp( "@", c, 1 ) ) fprintf( out, "%s%s", c, pre );
+                        }
+                    }
+                    else
+                    {
+                        switch ( pre[0] )
+                        {
+                        case '~':
+                            if ( DEBUG ) fprintf( stderr, "@%s", pre );
+                            loop = 0;
+                            break;
+                        case 'D':
+                        case 'd':
+                            if ( 0 == strncmp( "DROP",      pre, strlen( "DROP"      ) ) ) break;
+                            if ( 0 == strncmp( "drop",      pre, strlen( "drop"      ) ) ) break;
+                            if ( 0 == strncmp( "delimiter", pre, strlen( "delimiter" ) ) ) break;
+                        default:
+                            if ( 0 != strncmp( "@", c, 1 ) ) fprintf( out, "%s%s", c, pre );
+                        }
+                    }
+                    free( pre );
+                } else {
+                    loop = 0;
+                }
+            }
+            while ( loop );
 
-					if ( n )
-					{
-						return line;
-					}
-					else
-					{
-						free( line );
-						return NULL;
-					}
-				}
+            fclose( dev_null );
+        }
+
+            char* readline( FILE* stream )
+            {
+                int   n = 0;
+                char* line = calloc( 1024, sizeof(char) );
+                char* ptr  = calloc( 10,  sizeof(char) );
+                int read;
+                do
+                {
+                    read = fread( ptr, sizeof(char), 1, stream );
+                    if ( read )
+                    {
+                        switch( *ptr )
+                        {
+                        case '\n':
+                            line[n++] = *ptr;
+                            read = 0;
+                            break;
+                        default:
+                            line[n++] = *ptr;
+                        }
+                    }
+                } while ( 0 != read );
+                free( ptr );
+
+                if ( n )
+                {
+                    return line;
+                }
+                else
+                {
+                    free( line );
+                    return NULL;
+                }
+            }
 
